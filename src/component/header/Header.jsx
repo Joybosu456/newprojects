5ce7c2d615b556f6bb7ceceb5e6a5dcbe9832e2b
joy basu <joy.basu@bajajfinserv.in>
2024-10-16 10:48:53 +0530

    small changes

---
+++ src/component/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../header/header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div>
+            logo
+        </div>
+        <div className='header-right'>
+            <div>lang</div>
+            <div>headphone</div>
+            <div>call</div>
+            <div>gmail</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
